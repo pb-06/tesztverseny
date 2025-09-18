b5dff9ec5452a10fa82579c0028e729729e09234
pb-06 <kkandomail@gmail.com>
2025-09-18 14:12:52 +0200

    Find 1st place winner

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     private static TesztversenyHandler handler;
@@ -42,6 +42,12 @@ public class Main {
         handler.calculatePoints();
         handler.pointsToFile("pontok.txt");
         System.out.println();
+
+        Set<Integer> pointsSet = handler.pointsSet();
+        List<Integer> pointsList = new ArrayList<>(pointsSet);
+        pointsList.sort((e1, e2) -> e2-e1);
+        System.out.println("7. feladat: A verseny legjobbjai:");
+        handler.printWinners(pointsList.get(0), pointsList.get(1), pointsList.get(2));
 
         debugger();
     }
--- src/TesztversenyHandler.java
+++ src/TesztversenyHandler.java
@@ -2,8 +2,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.Set;
 
 public class TesztversenyHandler {
     private String helyes;
@@ -134,5 +136,22 @@ public class TesztversenyHandler {
             fwki.write(valaszok[i][0]+";"+valaszok[i][2]+"\n");
         }
         fwki.close();
+    }
+
+    public Set<Integer> pointsSet() {
+        Set<Integer> result = new HashSet<>();
+        for (int i=0; i<getValaszokDb(); i++) {
+            result.add(Integer.parseInt(valaszok[i][2]));
+        }
+        return result;
+
+    }
+
+    public void printWinners(int points1, int points2, int points3) {
+        for (int i=0; i<getValaszokDb(); i++) {
+            if (Integer.parseInt(valaszok[i][2])==points1) {
+                System.out.println("1. díj ("+points1+" pont): "+valaszok[i][0]);
+            }
+        }
     }
 }
